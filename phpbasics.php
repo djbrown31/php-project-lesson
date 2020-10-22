65feebf66574fb037996f69c6eca1120cd39e91a
Dan Brown <brown.danny.j@gmail.com>
2020-10-22 13:52:43 -0500

    First Commit Sublime Text Git Practice

---
+++ phpbasics.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PHP Basics</title>
+</head>
+<body>
+	<?php
+	$myArray = array("Hello", "Goodbye", "Good Day");
+
+	testFunction();
+	function testFunction() {
+		echo "This test Function Works.";
+	}
+	?>
+	
+</body>
+</html>
